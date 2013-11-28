dd2d2d540dda96892bb3d35242907323c56f7d9f
Jefry Lagrange <jefry.reyes@gmail.com>
2013-11-28 17:14:34 +0000

    minor changes

--- src/bitmessageqt/bitmessage_icons_rc.py
+++ src/bitmessageqt/bitmessage_icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Nov 27 23:35:32 2013
+# Created: Thu Nov 28 17:14:22 2013
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
